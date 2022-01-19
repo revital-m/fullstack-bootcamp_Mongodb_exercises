e9679e520bfba21038c1ec24bef7526ac53c5887
revital-m <revital13m@gmail.com>
2022-01-19 09:12:27 +0200

    ex 17.1

--- 17_1_products/index.js
+++ 17_1_products/index.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+const connectionURL = "mongodb://127.0.0.1:27017/e-commerce-api";
+
+mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+const Product = mongoose.model('Product', {
+    name: {
+        type: String,
+        
+    }
+});
